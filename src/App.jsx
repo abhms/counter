d6c6c123c9f24ff0c0578dfb8c10cd831dde9133
Abhishek Mishra <65820065+abhms@users.noreply.github.com>
2021-09-03 15:53:19 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+const App = () => {
+  // const state = useState();
+  const [count, setCount] = useState(0);
+
+  const IncNum = () => {
+    setCount(count + 1);
+  };
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={IncNum}>Click Me</button>
+    </div>
+  );
+};
+
+export default App;
